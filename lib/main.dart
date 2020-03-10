b942c296a23c23798c113142ed465bd61ec0d51e
Aaron Williams <>
2020-03-10 06:42:38 -0400

    Updated dynamic scaling to use LayoutBuilder.
    TODO: make the adjustment of notch cnt better, right now just uses (_wheelRadius * 0.25) on line 109 in scroll_wheel.dart. Also the dynamic sizing of the notches could use some worl

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'scroll_wheel.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Scroll Wheel Demo',
       theme: ThemeData(
-        brightness: Brightness.dark,
         primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
       ),
       home: MyHomePage(title: 'Scroll Wheel Demo'),
     );
@@ -19,7 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
   final String title;
 
   @override
@@ -27,21 +30,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
-
   @override
   Widget build(BuildContext context) {
+    double _wheelDia =
+        MediaQuery.of(context).orientation == Orientation.portrait
+            ? MediaQuery.of(context).size.width * 0.8
+            : MediaQuery.of(context).size.height * 0.8;
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            
-           ScrollWheel(),
-            
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Container(
+              key: scrollWheelKey,
+              width: _wheelDia,
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  return ScrollWheel(
+                    diameter: _wheelDia,
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/scroll_wheel.dart
+++ lib/scroll_wheel.dart
@@ -4,8 +4,7 @@ import 'dart:math';
 
 import 'package:flutter/services.dart';
 
-double _screenWidth;
-double _screenHeight;
+GlobalKey scrollWheelKey = GlobalKey();
 
 double _wheelPos = 0;
 
@@ -18,11 +17,14 @@ double _lastPos = 0;
 
 final double scaleFactor = 0.9;
 
-
 class ScrollWheel extends StatefulWidget {
+  final double diameter;
+
   ScrollWheel({
-    Key key, 
+    Key key,
+    this.diameter,
   }) : super(key: key);
+
   @override
   _ScrollWheelState createState() => _ScrollWheelState();
 }
@@ -31,12 +33,12 @@ class _ScrollWheelState extends State<ScrollWheel> {
   double _wheelThickness = 0;
   double _wheelRadius = 0;
 
-  double _getTheta(double _x, double _y) {
-    double _screenXCenter = _screenWidth / 2;
-    double _screenYCenter = _screenHeight / 2;
+  double _wheelCenterX = 0;
+  double _wheelCenterY = 0;
 
-    double _dx = _x - _screenXCenter;
-    double _dy = _y - _screenYCenter;
+  double _getTheta(double _x, double _y) {
+    double _dx = _x - _wheelCenterX;
+    double _dy = _y - _wheelCenterY;
 
     double _th = atan2(_dy, _dx);
     _th = _th + (90 * pi / 180);
@@ -55,8 +57,23 @@ class _ScrollWheelState extends State<ScrollWheel> {
     return deg * pi / 180;
   }
 
-  void _panHandler(DragUpdateDetails d) {
+  void _startPan(DragStartDetails d) {
+    final RenderBox _scrollwheelRender =
+        scrollWheelKey.currentContext.findRenderObject();
+    final _positionScrollWheel = _scrollwheelRender.localToGlobal(Offset.zero);
+
+    _wheelCenterX = _positionScrollWheel.dx + _wheelRadius;
+    _wheelCenterY = _positionScrollWheel.dy + _wheelRadius;
+
+    _startTouchPos =
+        _radToDeg(_getTheta(d.globalPosition.dx, d.globalPosition.dy));
+    _offsetTouchPos = (_startTouchPos - _lastTouchPos);
+  }
+
+  void _panUpdate(DragUpdateDetails d) {
     _currTouchPos = _getTheta(d.globalPosition.dx, d.globalPosition.dy);
+
+    print(_radToDeg(_currTouchPos));
 
     _updateWheelPos(_degToRad((_radToDeg(_currTouchPos) - _offsetTouchPos)));
 
@@ -69,6 +86,16 @@ class _ScrollWheelState extends State<ScrollWheel> {
     }
   }
 
+  void _endPan(DragEndDetails d) {
+    double _velocity = d.velocity.pixelsPerSecond.distance;
+    if (_lastTouchPos < _radToDeg(_wheelPos)) {
+      print("clockwise");
+    } else {
+      print("counterclockwise");
+    }
+    _lastTouchPos = _radToDeg(_wheelPos);
+  }
+
   void _updateWheelPos(double _pos) {
     setState(() {
       _wheelPos = _pos;
@@ -77,48 +104,107 @@ class _ScrollWheelState extends State<ScrollWheel> {
 
   @override
   Widget build(BuildContext context) {
-    _screenWidth = MediaQuery.of(context).size.width;
-    _screenHeight = MediaQuery.of(context).size.height;
-    _wheelRadius = MediaQuery.of(context).orientation == Orientation.portrait
-        ? (_screenWidth * scaleFactor) / 2
-        : (_screenHeight * scaleFactor) / 2;
-    double _wheelDia = _wheelRadius * 2;
+    double _wheelDia = widget.diameter;
+    _wheelRadius = _wheelDia / 2;
     double _notches = (_wheelRadius * 0.25).roundToDouble();
-    _wheelThickness = _wheelRadius * 0.3;
+    _wheelThickness = _wheelRadius * 0.25;
 
-    return GestureDetector(
-      onPanStart: (d) {
-        _startTouchPos =
-            _radToDeg(_getTheta(d.globalPosition.dx, d.globalPosition.dy));
-        _offsetTouchPos = (_startTouchPos - _lastTouchPos);
-      },
-      onPanUpdate: _panHandler,
-      onPanEnd: (d) {
-        double _velocity = d.velocity.pixelsPerSecond.distance;
-        if (_lastTouchPos < _radToDeg(_wheelPos)) {
-          print("clockwise");
-        } else {
-          print("counterclockwise");
-        }
-        _lastTouchPos = _radToDeg(_wheelPos);
-      },
-      child: Container(
-        alignment: Alignment.center,
-        width: _wheelDia,
-        height: _wheelDia,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-        ),
-        child: Transform.rotate(
-          angle: _wheelPos,
-          child: CustomPaint(
-            painter: new ScrollWheelPainter(
-                _wheelRadius, _wheelThickness, _notches),
-            willChange: false,
-            isComplex: true,
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        // Outside outer border
+        Container(
+          width: _wheelDia,
+          height: _wheelDia,
+          decoration: BoxDecoration(
+            color: Color(0xFF333333),
+            shape: BoxShape.circle,
+            border: Border.all(width: 0.5, color: Colors.black),
+            boxShadow: [
+              // Upper shadow light
+              BoxShadow(
+                color: Color(0xFF666666).withOpacity(0.5),
+                blurRadius: 10.0,
+                spreadRadius: 5.0,
+                offset: Offset(0.0, -5.0),
+              ),
+              // Lower shadow dark
+              BoxShadow(
+                color: Color(0xFF000000).withOpacity(0.8),
+                blurRadius: 10.0,
+                spreadRadius: 5.0,
+                offset: Offset(0.0, 5.0),
+              ),
+            ],
           ),
         ),
-      ),
+
+        // Scroll wheel painter
+        GestureDetector(
+          onPanStart: _startPan,
+          onPanUpdate: _panUpdate,
+          onPanEnd: _endPan,
+          child: Container(
+            alignment: Alignment.center,
+            width: _wheelDia * 0.95,
+            height: _wheelDia * 0.95,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              shape: BoxShape.circle,
+            ),
+            child: Transform.rotate(
+              angle: _wheelPos,
+              child: CustomPaint(
+                painter: new ScrollWheelPainter(
+                    _wheelRadius * 0.95, _wheelThickness, _notches),
+                willChange: false,
+                isComplex: true,
+              ),
+            ),
+          ),
+        ),
+
+        Container(
+          alignment: Alignment.center,
+          width: _wheelDia * 0.65,
+          height: _wheelDia * 0.65,
+          decoration: BoxDecoration(
+            color: Color(0xFF303030),
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xFF202020),
+                blurRadius: 2.0,
+                spreadRadius: 6.0,
+                offset: Offset(0.0, -2.0),
+              ),
+              BoxShadow(
+                color: Color(0xFF252525),
+                blurRadius: 2.0,
+                spreadRadius: 6.0,
+                offset: Offset(0.0, 2.0),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: _wheelDia * 0.6,
+          height: _wheelDia * 0.6,
+          decoration: BoxDecoration(
+            color: Color(0xFF202020),
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xFF000000),
+                blurRadius: 10.0,
+                spreadRadius: 0.0,
+                offset: Offset(0.0, 0.0),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
@@ -145,37 +231,46 @@ class ScrollWheelPainter extends CustomPainter {
     canvas.drawCircle(Offset(0, 0), _whRad - (_whThk / 2), paint);
 
     // paint wheel borders
-    paint.color = Color(0xFF0d0d0d);
+    paint.color = Colors.black;
     paint.style = PaintingStyle.stroke;
-    paint.strokeWidth = 2;
-    canvas.drawCircle(Offset(0, 0), _whRad + 1, paint);
+    paint.strokeWidth = 3;
+    canvas.drawCircle(Offset(0, 0), _whRad, paint);
     canvas.drawCircle(Offset(0, 0), _whRad - _whThk, paint);
 
     paint.strokeCap = StrokeCap.round;
 
+    double _line0Stroke = 10.0;
+    double _line0Y0 = ((_whRad - _whThk) + _line0Stroke / 2) + 2;
+    double _line0Y1 = (_whRad - _line0Stroke / 2) - 2;
+
+    double _line1Stroke = 6.0;
+    double _line1Y0 = ((_whRad - _whThk) + _line1Stroke / 2) + 3;
+    double _line1Y1 = (_whRad - _line1Stroke / 2) - 3;
+
+    double _line2Stroke = 4.0;
+    double _line2Y0 = ((_whRad - _whThk) + _line2Stroke / 2) + 5;
+    double _line2Y1 = (_whRad - _line2Stroke / 2) - 5;
+
     for (int i = 0; i < _notchCnt; i++) {
       canvas.rotate(_notchRad);
 
-      paint.color = Color(0xFF080808);
-      paint.strokeWidth = 11.0;
-      canvas.drawLine(
-          Offset(0, (_whRad - _whThk) + 5), Offset(0, _whRad - 5), paint);
+      paint.color = Color(0xFF000000);
+
+      paint.strokeWidth = _line0Stroke;
+      canvas.drawLine(Offset(0, _line0Y0), Offset(0, _line0Y1), paint);
+
+      paint.color = Color(0xFF1a1a1a);
+      paint.strokeWidth = _line1Stroke;
+      canvas.drawLine(Offset(0, _line1Y0), Offset(0, _line1Y1), paint);
 
       paint.color = Color(0xFF202020);
-      paint.strokeWidth = 7.0;
-      canvas.drawLine(
-          Offset(0, (_whRad - _whThk) + 6), Offset(0, _whRad - 6), paint);
-
-      paint.color = Color(0xFF121212);
-      paint.strokeWidth = 5.0;
-      canvas.drawLine(
-          Offset(0, (_whRad - _whThk) + 6), Offset(0, _whRad - 6), paint);
+      paint.strokeWidth = _line2Stroke;
+      canvas.drawLine(Offset(0, _line2Y0), Offset(0, _line2Y1), paint);
     }
-
-    paint.color = Colors.white.withOpacity(0.2);
-    paint.strokeWidth = 4.0;
-    canvas.drawLine(
-        Offset(0, (_whRad - _whThk) + 7), Offset(0, _whRad - 7), paint);
+    canvas.rotate(pi);
+    paint.color = Colors.white30;
+    paint.strokeWidth = 3.0;
+    canvas.drawLine(Offset(0, _line2Y0), Offset(0, _line2Y1), paint);
   }
 
   @override
